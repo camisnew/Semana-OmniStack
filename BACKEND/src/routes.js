ef677c7d6f790ee7e56fc3524733c864d60281e7
camisnew <camilabertholdpires@gmail.com>
2020-03-24 22:45:56 -0300

    rotas de incidents funcionais

--- BACKEND/src/routes.js
+++ BACKEND/src/routes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
-const OngController = require('./controllers/OngController')
+const ongController = require('./controllers/OngController')
+const incidentController = require('./controllers/IncidentController')
 
 
 const routes = express.Router();
 
-routes.get('/ongs', OngController.index);
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', ongController.create);
 
-routes.post('/ongs', OngController.create);
+routes.get('/incidents', incidentController.index);
+routes.post('/incidents', incidentController.create);
+routes.delete('/incidents/:id', incidentController.delete);
 
 module.exports = routes;
